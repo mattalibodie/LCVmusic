1da45408a6a7032c0974e91870fe806d3473164b
ngluong <nguyenluongtt2@gmail.com>
2023-11-08 17:44:43 +0700

    add css, add sign in page, register page, singer browse page, add more song

--- js/song.js
+++ js/song.js
@@ -24,7 +24,7 @@ const musics = [ // Cập Nhật Nhạc
         id: 3,
         title: "Vườn Nhà",
         singer: "Phan Mạnh Quỳnh",
-        file: "TroThanhPhuThe-HuongLy-10710826.mp3",
+        file: "VuonNha-PhanManhQuynh-9777057.mp3",
         image: "image/album/demo_vuon_nha.jpg"
     },
     {
@@ -40,5 +40,40 @@ const musics = [ // Cập Nhật Nhạc
         singer: "Min",
         file: "Ca-Phe-MIN.mp3",
         image: "image/album/cafe-min.png"
+    },
+    {
+        id: 6,
+        title: "Chúng ta của hiện tại",
+        singer: "Sơn Tùng M-TP",
+        file: "chung_ta_cua_hien_tai.mp3",
+        image: "image/album/chungtacuahientai.jpg"
+    },
+    {
+        id: 7,
+        title: "Âm Thầm Bên Em",
+        singer: "Sơn Tùng M-TP",
+        file: "am_tham_ben_em.mp3",
+        image: "image/album/am_tham_ben_em.jpg"
+    },
+    {
+        id: 8,
+        title: "Có Chắc Yêu Là Đây",
+        singer: "Sơn Tùng M-TP",
+        file: "co_chac_yeu_la_day.mp3",
+        image: "image/album/co_chac_yeu_la_day.png"
+    },
+    {
+        id: 9,
+        title: "Lạc Trôi",
+        singer: "Sơn Tùng M-TP",
+        file: "lac_troi.mp3",
+        image: "image/album/lac_troi.jpg"
+    },
+    {
+        id: 10,
+        title: "Muộn rồi mà sao còn",
+        singer: "Sơn Tùng M-TP",
+        file: "MuonRoiMaSaoCon-SonTungMTP-7011803.mp3",
+        image: "image/album/muon_roi_ma_sao_con.jpg"
     },
 ];
