5a0f05f12e1d38d148913dc747a14bf1b99eee24
ngluong <nguyenluongtt2@gmail.com>
2023-11-11 17:11:33 +0700

    no change

--- js/gallery.js
+++ js/gallery.js
@@ -1,37 +1,50 @@
-let slider = document.querySelector('.slider .list');
-let items = document.querySelectorAll('.slider .list .item');
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-let dots = document.querySelectorAll('.slider .dots li');
 
-let lengthItems = items.length - 1;
-let active = 0;
-next.onclick = function(){
-    active = active + 1 <= lengthItems ? active + 1 : 0;
-    reloadSlider();
-}
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active - 1 : lengthItems;
-    reloadSlider();
-}
-let refreshInterval = setInterval(()=> {next.click()}, 3000);
-function reloadSlider(){
-    slider.style.left = -items[active].offsetLeft + 'px';
-    let last_active_dot = document.querySelector('.slider .dots li.active');
-    last_active_dot.classList.remove('active');
-    dots[active].classList.add('active');
-    clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000);
+var galleryContent = [
+    {
+        "id" : "0",
+        "photo" : "./image/slide_show/gallery.jpg"
+    },
+    {
+        "id" : "1",
+        "photo" : "./image/slide_show/gallery1.jpg"
+    },
+    {
+        "id" : "2",
+        "photo" : "./image/slide_show/gallery2.jpg"
+    },
+    {
+        "id" : "3",
+        "photo" : "./image/slide_show/gallery3.jpg"
+    },
+    {
+        "id" : "4",
+        "photo" : "./image/slide_show/gallery4.jpg"
+    },
+]
 
+function init()
+{
+    for(var i = 0; i < galleryContent.length; i++)
+    {
+        var item = galleryContent[i];
+        var dc=$("<div>");
+        $("<img src='" + item.photo+ "'/>").appendTo(dc);
+        $("#gallery").append(dc);
+    }
 }
 
-dots.forEach((li, key) => {
-    li.addEventListener('click', ()=>{
-         active = key;
-         reloadSlider();
-    })
-})
-window.onresize = function(event) {
-    reloadSlider();
+
+function galleryEffect()
+{
+    init();
+    $("#gallery > div:gt(0)").hide();
+    setInterval(function(){
+        $('#gallery > div:first').hide().next().fadeIn(1000).end().appendTo('#gallery');  
+    }, 4500);
 };
+$(document).ready(function()
+{
+    galleryEffect();
+})
+
 
