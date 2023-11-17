681c5540233e2fa404119634cb0de84e7a8292da
ngluong <nguyenluongtt2@gmail.com>
2023-11-17 19:46:42 +0700

    initial commit

--- js/song.js
+++ js/song.js
@@ -2,56 +2,56 @@ const musics = [ // Cập Nhật Nhạc
     {
         id: 0,
         title: "Making My Way",
-        singer: "Sơn Tùng M-TP",
+        singer: "Sơn Tùng M-TP.",
         file: "making_my_way.mp3",
         image: "image/album/demo_making_my_way.jpg"
     },
     {
         id: 1,
         title: "Trở Thành Phu Thê",
-        singer: "Hương Ly",
+        singer: "Hương Ly.",
         file: "TroThanhPhuThe-HuongLy-10710826.mp3",
         image: "image/album/troThanhPhuThue.jpg"
     },
     {
         id: 2,
         title: "Đợi (2023 remake)",
-        singer: "Vũ",
+        singer: "Vũ.",
         file: "Doi-Vu-remake.mp3",
         image: "image/album/demo_vu_doi_2023.jpg"
     },
     {
         id: 3,
         title: "Lạ Lùng",
-        singer: "Vũ",
+        singer: "Vũ.",
         file: "la_lung.mp3",
         image: "image/album/la_lung.jpg"
     },
     {
         id: 4,
         title: "Từng Quen",
-        singer: "Wren Evan",
+        singer: "Wren Evan.",
         file: "Tung_Quen_Wren_Evan.mp3",
         image: "image/album/demo_tungquen.jpg"
     },
     {
         id: 5,
         title: "Cà Phê",
-        singer: "Min",
+        singer: "Min.",
         file: "Ca-Phe-MIN.mp3",
         image: "image/album/cafe-min.png"
     },
     {
         id: 6,
         title: "Chúng ta của hiện tại",
-        singer: "Sơn Tùng M-TP",
+        singer: "Sơn Tùng M-TP.",
         file: "chung_ta_cua_hien_tai.mp3",
         image: "image/album/chungtacuahientai.jpg"
     },
     {
         id: 7,
         title: "Âm Thầm Bên Em",
-        singer: "Sơn Tùng M-TP",
+        singer: "Sơn Tùng M-TP.",
         file: "am_tham_ben_em.mp3",
         image: "image/album/am_tham_ben_em.jpg"
     },
